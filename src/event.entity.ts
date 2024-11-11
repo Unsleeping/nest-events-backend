86b5640616b566b53857d4dcc215e26ee031fe41
Alexander Mandrov <otvet-1995@mail.ru>
2024-11-11 13:48:23 +0200

    init typeORM

--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,13 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
 import { CreateEventDto } from './create-event.dto';
 
 interface EventBase extends Omit<CreateEventDto, 'when'> {
   when: Date;
 }
 
+@Entity()
 export class Event implements EventBase {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   when: Date;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   address: string;
 }
