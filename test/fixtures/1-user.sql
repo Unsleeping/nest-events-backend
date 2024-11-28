1f411a6cda3369a0a4f19eae2651636569f13265
Alexander Mandrov <otvet-1995@mail.ru>
2024-11-28 18:18:11 +0200

    add more e2e tests and fixtures over the events module

---
+++ test/fixtures/1-user.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+    `user` (
+        `id`,
+        `username`,
+        `password`,
+        `email`,
+        `firstName`,
+        `lastName`,
+        `profileId`
+    )
+VALUES
+    (
+        NULL,
+        'e2e-test',
+        '<reducted>',
+        'e2e@test.com',
+        'End',
+        'To End',
+        NULL
+    )
