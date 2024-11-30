c20d37c93b65a7a98532d6ee3c86b0eda574d47d
Alexander Mandrov <otvet-1995@mail.ru>
2024-11-30 11:50:54 +0200

    extract auth logic for reusability

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
   public getTokenForUser(user: User): string {
     return this.jwtService.sign({
@@ -17,5 +25,23 @@ export class AuthService {
 
   public async hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, 10);
+  }
+
+  public async validateUser(username: string, password: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { username } });
+
+    if (!user) {
+      this.logger.debug(`User not found: ${username}`);
+      throw new UnauthorizedException();
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      this.logger.debug(`Invalid password for user: ${username}`);
+      throw new UnauthorizedException();
+    }
+
+    return user;
   }
 }
