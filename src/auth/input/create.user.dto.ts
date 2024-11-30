6bc1b8a75b4d53e61e7e3e05f8253f5b9988dd55
Alexander Mandrov <otvet-1995@mail.ru>
2024-11-30 14:01:13 +0200

    add userAdd mutation

--- src/auth/input/create.user.dto.ts
+++ src/auth/input/create.user.dto.ts
@@ -1,27 +1,35 @@
 import { IsEmail, Length } from 'class-validator';
+import { Field, InputType } from '@nestjs/graphql';
 
 import { IsRepeated } from '../../validation/is-repeated.constraint';
 import { UserDoesNotExist } from '../validation/user-does-not-exist.constraint';
 
+@InputType('UserAddInput')
 export class CreateUserDto {
   @Length(5)
   @UserDoesNotExist()
+  @Field()
   username: string;
 
   @Length(8)
+  @Field()
   password: string;
 
   @Length(8)
   @IsRepeated('password')
+  @Field()
   retypedPassword: string;
 
   @Length(2)
+  @Field()
   firstName: string;
 
   @Length(2)
+  @Field()
   lastName: string;
 
   @IsEmail()
   @UserDoesNotExist()
+  @Field()
   email: string;
 }
